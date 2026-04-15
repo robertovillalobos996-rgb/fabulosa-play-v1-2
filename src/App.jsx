0bc0ad1baec58f6069c8cb4fb359a7859059cafa
robertovillalobos996-rgb <robertovillalobos996@gmail.com>
2026-04-15 15:14:43 -0600

    Reparación quirúrgica: Rutas /cine-play y /canales-play habilitadas, logo transparente y fix YouTube

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { pagesConfig } from './pages.config';
 
 import SecurityLock from './components/SecurityLock';
-import CentroMercadeo from './pages/CentroMercadeo';
-import RancheraPlay from './pages/RancheraPlay'; 
-import ReporteMensual from './pages/ReporteMensual';
+import Home from './pages/Home';
+import CinePlay from './pages/Movies'; // Asegúrate de que el archivo se llame Movies o CinePlay
+import CanalesPlay from './pages/Channels'; // Asegúrate de que el archivo se llame Channels o CanalesPlay
+import FabulosaAlabanza from './pages/FabulosaAlabanza';
 
 const queryClient = new QueryClient();
-
-const { Pages, mainPage } = pagesConfig;
-const mainPageKey = mainPage ?? Object.keys(Pages)[0];
-const MainPage = Pages[mainPageKey];
 
 function App() {
   return (
@@ -20,12 +17,15 @@ function App() {
       <SecurityLock />
       <Router>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/centro-mercadeo" element={<CentroMercadeo />} />
-          <Route path="/cantina" element={<RancheraPlay />} />
-          <Route path="/reporte" element={<ReporteMensual />} />
+          <Route path="/" element={<Home />} />
+          
+          {/* RUTAS CORREGIDAS PARA QUE COINCIDAN CON LAS CARDS */}
+          <Route path="/cine-play" element={<CinePlay />} />
+          <Route path="/canales-play" element={<CanalesPlay />} />
+          <Route path="/alabanza" element={<FabulosaAlabanza />} />
 
-          {Object.entries(Pages).map(([path, Page]) => (
+          {/* Mantenimiento de las demás rutas automáticas */}
+          {Object.entries(pagesConfig.Pages).map(([path, Page]) => (
             <Route key={path} path={`/${path}`} element={<Page />} />
           ))}
         </Routes>
