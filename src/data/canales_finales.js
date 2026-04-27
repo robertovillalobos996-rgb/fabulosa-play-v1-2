304d5891393acdb876ffc6aa99c5bb879a1ccde2
robertovillalobos996-rgb <robertovillalobos996@gmail.com>
2026-04-26 21:55:46 -0600

    Canal 6 con link funcional y orden de STV corregido

--- src/data/canales_finales.js
+++ src/data/canales_finales.js
@@ -21,6 +21,13 @@ export const canalesTV = [
         "logo": "/logos_canales/88_Stereo.png"
     },
     {
+        "id": "canal-6-repretel",
+        "title": "Canal 6",
+        "url": "https://alba-cr-repretel-c6.stream.mediatiquestream.com/index.m3u8",
+        "genre": "Costa Rica",
+        "logo": "/logos_canales/Repretel_6_logo.png"
+    },
+    {
         "id": "canal-6",
         "title": "Canal 6",
         "url": "https://d2qsan2ut81n2k.cloudfront.net/live/02f0dc35-8fd4-4021-8fa0-96c277f62653/ts:abr.m3u8",
