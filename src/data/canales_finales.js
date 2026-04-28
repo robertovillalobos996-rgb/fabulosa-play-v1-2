9f21b46db283538c23fd43ea2035fcb15221a1ae
robertovillalobos996-rgb <robertovillalobos996@gmail.com>
2026-04-28 11:13:51 -0600

    Cirugia Repretel 6: Usando iframe oficial para saltar bloqueo

--- src/data/canales_finales.js
+++ src/data/canales_finales.js
@@ -14,6 +14,14 @@ export const canalesTV = [
         "logo": "/logos_canales/stv.png"
     },
     {
+        "id": "tv-6-repretel",
+        "title": "Repretel Canal 6",
+        "iframe_url": "https://www.repretel.com/en-vivo/canal-6/",
+        "tipo": "webview_recortado",
+        "genre": "Costa Rica",
+        "logo": "/logos_canales/Repretel_6_logo.png"
+    },
+    {
         "id": "tv-7-teletica",
         "title": "Teletica Canal 7",
         "iframe_url": "https://bradmax.com/client/embed-player/c7c83ebb46fa89529a7383d933e2038729f8e4c9_13428?id=tv7&mediaUrl=https://cdn01.teletica.com/TeleticaLiveStream/Stream/playlist_dvr.m3u8",
@@ -26,13 +34,6 @@ export const canalesTV = [
         "iframe_url": "https://www.dailymotion.com/embed/video/x7vh8g3?autoplay=1",
         "genre": "Costa Rica",
         "logo": "/logos_canales/canal_13.jpg"
-    },
-    {
-        "id": "tv-36-trivision",
-        "title": "Trivisión Canal 36",
-        "link_externo": "https://player.instantvideocloud.net/v2/channel/1591",
-        "genre": "Costa Rica",
-        "logo": "/logos_canales/trivision.png"
     },
     {
         "id": "tv-2",
