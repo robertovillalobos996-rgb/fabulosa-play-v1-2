59861af8ae307d1989c641b60fd76cc8dde2019e
robertovillalobos996-rgb <robertovillalobos996@gmail.com>
2026-04-30 09:09:35 -0600

    Corregida posición 6 de Radios Costa Rica

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,37 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// 1. IMPORTACIÓN DE TODAS SUS PÁGINAS (Verificadas en su carpeta pages)
+// 1. IMPORTACIÓN DE TODAS SUS PÁGINAS (Nombres exactos de sus archivos)[cite: 5]
 import Home from './pages/Home';
 import PremiumHub from './pages/PremiumHub';
 import FabulosaTube from './pages/FabulosaTube';
 import RancheraPlay from './pages/RancheraPlay';
-import Radio from './pages/Radio';
+import Radio from './pages/Radio'; // Este es el de Costa Rica
 import Movies from './pages/Movies';
 import Channels from './pages/Channels';
 import Karaoke from './pages/Karaoke';
 import FabulosaAlabanza from './pages/FabulosaAlabanza';
 import Camaras from './pages/Camaras';
 import CentroMercadeo from './pages/CentroMercadeo';
-import FabulosaTV from './pages/FabulosaTV'; // Usada para la ruta /tv-1
-import AdminPanel from './AdminPanel'; // Su panel de control
+import FabulosaTV from './pages/FabulosaTV'; 
+import AdminPanel from './AdminPanel';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* RUTA PRINCIPAL: DONDE ESTÁN LAS 12 CARDS */}
+        {/* PANTALLA DE INICIO (Las 12 Cards) */}
         <Route path="/" element={<Home />} />
 
-        {/* 2. CONEXIÓN EXACTA CON LOS ENLACES DE HOME.JSX */}
+        {/* 2. CONEXIÓN DE LAS 12 CARDS (Rutas exactas de Home.jsx)[cite: 3] */}
         <Route path="/premium" element={<PremiumHub />} />
         <Route path="/fabulosa-tube" element={<FabulosaTube />} />
         <Route path="/tv-1" element={<FabulosaTV />} />
         <Route path="/ranchera" element={<RancheraPlay />} />
+        
+        {/* POSICIÓN 6: RADIOS DE TODO COSTA RICA */}
         <Route path="/radios-cr" element={<Radio />} />
+
         <Route path="/cine-play" element={<Movies />} />
         <Route path="/canales-play" element={<Channels />} />
         <Route path="/karaoke" element={<Karaoke />} />
@@ -37,7 +40,7 @@ function App() {
         <Route path="/camaras" element={<Camaras />} />
         <Route path="/centro-mercadeo" element={<CentroMercadeo />} />
         
-        {/* RUTA PARA SU PANEL DE ADMINISTRACIÓN */}
+        {/* RUTA DEL ADMINISTRADOR */}
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </BrowserRouter>
