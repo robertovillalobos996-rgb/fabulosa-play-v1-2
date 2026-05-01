8ec1f4dcc48886b7621dfd570a9ae51290a99aba
robertovillalobos996-rgb <robertovillalobos996@gmail.com>
2026-04-30 21:24:15 -0600

    Actualizando rutas de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,41 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// 1. IMPORTACIÓN DE PÁGINAS (Verificadas según sus archivos)[cite: 8, 9, 10]
-import Home from './pages/Home';
-import PremiumHub from './pages/PremiumHub';
-import RadioPremium from './pages/RadioPremium'; 
-import Radio from './pages/Radio'; // El archivo que filtra Costa Rica
-import Movies from './pages/Movies';
-import Channels from './pages/Channels';
-import FabulosaTube from './pages/FabulosaTube';
-import RancheraPlay from './pages/RancheraPlay';
-import Karaoke from './pages/Karaoke';
-import FabulosaAlabanza from './pages/FabulosaAlabanza';
-import Camaras from './pages/Camaras';
-import CentroMercadeo from './pages/CentroMercadeo';
-import FabulosaTV from './pages/FabulosaTV'; 
-import AdminPanel from './AdminPanel';
+// Todos los componentes importados desde la carpeta 'page'
+import Home from './page/Home';
+import PremiumHub from './page/PremiumHub';
+import FabulosaTube from './page/FabulosaTube';
+import RancheraPlay from './page/RancheraPlay';
+import Radio from './page/Radio';
+import Movies from './page/Movies';
+import Channels from './page/Channels';
+import Karaoke from './page/Karaoke';
+import FabulosaAlabanza from './page/FabulosaAlabanza';
+import Camaras from './page/Camaras';
+import CentroMercadeo from './page/CentroMercadeo';
+import FabulosaTV from './page/FabulosaTV';
+import AdminPanel from './page/AdminPanel';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        {/* PANTALLA PRINCIPAL (12 CARDS) */}
         <Route path="/" element={<Home />} />
-
-        {/* CARD 1: MUNDO VIP Y SUS SEÑALES[cite: 8, 10] */}
         <Route path="/premium" element={<PremiumHub />} />
-        {/* Estas rutas ahora sí abrirán el reproductor VIP */}
-        <Route path="/premium/voice-over" element={<RadioPremium />} />
-        <Route path="/premium/fabulosa-radio" element={<RadioPremium />} />
-
-        {/* CARD 6: RADIOS DE COSTA RICA (Corregida)[cite: 8, 9] */}
-        <Route path="/radios-cr" element={<Radio />} />
-
-        {/* RESTO DE LAS CARDS[cite: 8] */}
-        <Route path="/fabulosa-tube" element={<FabulosaTube />} />
-        <Route path="/tv-1" element={<FabulosaTV />} />
+        <Route path="/tube" element={<FabulosaTube />} />
         <Route path="/ranchera" element={<RancheraPlay />} />
-        <Route path="/cine-play" element={<Movies />} />
+        <Route path="/radio" element={<Radio />} />
+        <Route path="/movies" element={<Movies />} />
         <Route path="/canales-play" element={<Channels />} />
         <Route path="/karaoke" element={<Karaoke />} />
         <Route path="/alabanza" element={<FabulosaAlabanza />} />
         <Route path="/camaras" element={<Camaras />} />
-        <Route path="/centro-mercadeo" element={<CentroMercadeo />} />
-        
-        {/* PANEL DE ADMINISTRACIÓN */}
+        <Route path="/mercadeo" element={<CentroMercadeo />} />
+        <Route path="/tv" element={<FabulosaTV />} />
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
